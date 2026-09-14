af8d4f00556d714374793f67bcfc910b32d8e30c
Thifjj <thifj.fj@gmail.com>
2026-09-14 18:50:01 -0300

    adding results from zcu104

---
+++ results_all_models/methane_mobilenet_v3/reproduce_best.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+'/home/root/Benchmark_ZCU104/benchmark_zcu104' --profile max-e2e --model '/home/root/models/compiled_zcu104/mobilenet_v3/methane_mobilenet_v3.xmodel' --dataset '/home/root/STARCOP_test' --csv '/home/root/STARCOP_test/test.csv' --out '/home/root/Benchmark_ZCU104/results_all_models/methane_mobilenet_v3/best_reproduction' --runners 2 --pre-workers 1 --post-workers 1 --slots-per-runner 1 --iterations 0 --samples 0 --cpu-cores 4 --warmup 2 --no-pin --no-validate
